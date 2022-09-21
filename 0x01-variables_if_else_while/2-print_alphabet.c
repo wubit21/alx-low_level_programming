bb9d1b81cfe8e59b5df43c668e4c023f81ea9c70
wubit21 <wubitade21@gmail.com>
2022-09-21 14:44:40 -0700

    if else while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase
+ *
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
